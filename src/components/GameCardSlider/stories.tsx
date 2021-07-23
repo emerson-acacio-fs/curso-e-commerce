e48962b1aadc85981d0a1df757a1059edd14fd0a
emerson_acacio@lccv.ufal.br <emerson_acacio@lccv.ufal.br>
2021-07-23 10:25:53 -0300

    conclusão do GameCardSlider

---
+++ src/components/GameCardSlider/stories.tsx
@@ -0,0 +1,69 @@
+import {Meta, Story} from '@storybook/react/types-6-0';
+
+import {GameCardSlider, GameCardSliderProps} from '.';
+import {GameCardProps} from '../GameCard';
+
+const items: GameCardProps[] = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x145',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+];
+
+const data = {
+  title: 'GameCardSlider',
+  component: GameCardSlider,
+  args: {items},
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark',
+    },
+  },
+} as Meta;
+
+export default data;
+
+export const Default: Story<GameCardSliderProps> = (args) => (
+  <div style={{maxWidth: '130rem', margin: '0 auto'}}>
+    <GameCardSlider {...args} />
+  </div>
+);
