fe049143f325252d40987b873ea29b9822772f3a
emerson_acacio@lccv.ufal.br <emerson_acacio@lccv.ufal.br>
2021-08-06 19:37:20 -0300

    conclusão do textContent

---
+++ src/components/GameDetails/index.tsx
@@ -0,0 +1,83 @@
+import {Heading} from 'components/Heading';
+import {Apple, Windows, Linux} from '@styled-icons/fa-brands';
+import {MediaMatch} from 'components/MediaMatch';
+import * as S from './styles';
+
+type Rating = 'BR0' | 'BR10' | 'BR12' | 'BR14' | 'BR16' | 'BR18';
+type Platform = 'windows' | 'linux' | 'mac';
+
+export type GameDetailsProps = {
+  developer: string;
+  platforms: Platform[];
+  releaseDate: string;
+  rating: Rating;
+  genres: string[];
+};
+
+const platformIcons = {
+  linux: <Linux title="Linux" size={18} />,
+  mac: <Apple title="Mac" size={18} />,
+  windows: <Windows title="Windows" size={18} />,
+};
+
+export const GameDetails = ({
+  developer,
+  releaseDate,
+  platforms,
+  rating,
+  genres,
+}: GameDetailsProps) => {
+  return (
+    <S.Wrapper>
+      <MediaMatch greaterThan="small">
+        <Heading lineLeft lineColor="secondary">
+          Game Details
+        </Heading>
+      </MediaMatch>
+
+      <S.Content>
+        <S.Block>
+          <S.Label>Developer</S.Label>
+          <S.Description>{developer}</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Release Date</S.Label>
+          <S.Description>
+            {new Intl.DateTimeFormat('en-US', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric',
+            }).format(new Date(releaseDate))}
+          </S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Platforms</S.Label>
+          <S.IconsWrapper>
+            {platforms.map((icon: Platform) => (
+              <S.Icon key={icon}>{platformIcons[icon]}</S.Icon>
+            ))}
+          </S.IconsWrapper>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Publisher</S.Label>
+          <S.Description>2K</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Rating</S.Label>
+          <S.Description>
+            {rating === 'BR0' ? 'FREE' : `${rating.replace('BR', '')}+`}
+          </S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Genres</S.Label>
+          <S.Description>{genres.join(' / ')}</S.Description>
+        </S.Block>
+      </S.Content>
+    </S.Wrapper>
+  );
+};
